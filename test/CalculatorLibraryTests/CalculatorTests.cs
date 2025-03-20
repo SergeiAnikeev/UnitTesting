5fe0c0be5cef046c83c298d36be7ef1e916f6ce9
Sergei Anikeev <anikeevsu@gmail.com>
2025-03-20 15:58:05 +1300

    IAsyncLifetime

--- test/CalculatorLibraryTests/CalculatorTests.cs
+++ test/CalculatorLibraryTests/CalculatorTests.cs
@@ -1,13 +1,59 @@
-﻿namespace CalculatorLibrary.Tests.Unit
+﻿using Xunit.Abstractions;
+
+namespace CalculatorLibrary.Tests.Unit
 {
-    public class CalculatorTests
+    public class CalculatorTests : IAsyncLifetime
     {
-        [Fact]
-        public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers ()
+        private readonly Calculator _sut = new();
+        private readonly Guid _guid = Guid.NewGuid();
+        private ITestOutputHelper _outputHelper;
+
+        public CalculatorTests(ITestOutputHelper outputHelper)
         {
-            var calculator = new Calculator();
-            var result = calculator.Add(1,2);
+            _outputHelper = outputHelper;
+            _outputHelper.WriteLine("Hello from Ctor");
+        }
+
+        [Fact]
+        public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
+        {
+            //Act
+            var result = _sut.Add(1,2);
+            
+            //Assert
             Assert.Equal(3, result);
+        }
+        [Fact]
+        public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers()
+        {
+            //Act
+            var result = _sut.Substract(5, 4);
+
+            //Assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Test_Guid_1()
+        {
+            _outputHelper.WriteLine(_guid.ToString());
+        }
+        [Fact]
+        public void Test_Guid_2()
+        {
+            _outputHelper.WriteLine(_guid.ToString());
+        }
+
+        public async Task InitializeAsync()
+        {
+            _outputHelper.WriteLine("Hello from InitializeAsync");
+            await Task.Delay(1);
+        }
+
+        public async Task DisposeAsync()
+        {
+            _outputHelper.WriteLine("Hello from DisposeAsync");
+            await Task.Delay(1);
         }
     }
 }
