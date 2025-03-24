476bbeb85e6aa114952b0a1d66b6d2abc93721eb
Sergei Anikeev <anikeevsu@gmail.com>
2025-03-25 09:43:32 +1300

    more examples

--- TestingTechniques/ValueSamples.cs
+++ TestingTechniques/ValueSamples.cs
@@ -13,5 +13,36 @@
             DateOfBirth = new(2000, 1, 1)
         };
 
+        public IEnumerable<User> Users = new[]
+        {
+            new User()
+            {
+                FullName = "John Jones",
+                Age = 25,
+                DateOfBirth = new(2000, 1, 1)
+            },
+            new User()
+            {
+                FullName = "John Jones 2",
+                Age = 26,
+                DateOfBirth = new(2001, 1, 1)
+            },
+            new User()
+            {
+                FullName = "John Jones 3",
+                Age = 35,
+                DateOfBirth = new(2002, 1, 1)
+            }
+        };
+
+
+        public IEnumerable<int> Numbers = new[] { 1, 2, 3, 4, 5, 6, };
+        public event EventHandler ExampleEvent;
+        internal int InternalSecretNumber = 42;
+        public virtual void RaiseExampleEvent()
+        {
+            ExampleEvent(this, EventArgs.Empty);
+        }
+
     };
 }
--- test/CalculatorLibraryTests/ValueSamplesTests.cs
+++ test/CalculatorLibraryTests/ValueSamplesTests.cs
@@ -45,7 +45,58 @@ namespace CalculatorLibrary.Tests.Unit
         [Fact]
         public void ObjectAssertionExample()
         {
+            var expected = new User
+            {
+                FullName = "John Jones",
+                Age = 25,
+                DateOfBirth = new(2000, 1, 1)
+            };
+
+            var user = _sut.AppUser;
+
+            //Assert.Equal(expected, user);
+            user.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void EnumerableAssertionExample()
+        {
+            var expected = new User
+            {
+                FullName = "John Jones",
+                Age = 25,
+                DateOfBirth = new(2000, 1, 1)
+            };
+
+            var users = _sut.Users.As<User[]>();
+
+            users.Should().ContainEquivalentOf(expected);
+            users.Should().HaveCount(3);
+            users.Should().Contain(x => x.FullName.StartsWith("John") && x.Age > 18);
 
         }
+
+
+        [Fact]
+        public void Exception_ThrownAsseretion()
+        {
+            var calculator = new Calculator();
+
+            Action result = () => calculator.Divide(1, 0);
+
+            result.Should().Throw<DivideByZeroException>().WithMessage("Attempted to divide by zero.");
+
+        }
+
+        [Fact]
+        public void EventRaisedAssertionExample()
+        {
+            var monitorSubject = _sut.Monitor();
+
+            _sut.RaiseExampleEvent();
+
+            monitorSubject.Should().Raise("ExampleEvent");
+        }
+
     }
 }
