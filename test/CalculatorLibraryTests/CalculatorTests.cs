2f337055892803b898126b3431a38fe86396912f
Sergei Anikeev <anikeevsu@gmail.com>
2025-03-24 14:51:26 +1300

    more tests

--- test/CalculatorLibraryTests/CalculatorTests.cs
+++ test/CalculatorLibraryTests/CalculatorTests.cs
@@ -14,31 +14,71 @@ namespace CalculatorLibrary.Tests.Unit
             _outputHelper.WriteLine("Hello from Ctor");
         }
 
-        [Fact]
-        public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
+        [Theory]
+        [InlineData(5, 5, 10)]
+        [InlineData(-5, 5, 0)]
+        [InlineData(-15, -5, -20)]
+        public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
         {
             //Act
-            var result = _sut.Add(1,2);
+            var result = _sut.Add(a,b);
             
             //Assert
-            Assert.Equal(3, result);
+            Assert.Equal(expected, result);
         }
-        [Fact]
-        public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers()
+
+        [Theory]
+        [InlineData(5, 5, 0)]
+        [InlineData(15, 5, 10)]
+        [InlineData(-5, -5, 0)]
+        [InlineData(-15, -5, -10)]
+        [InlineData(5, 10, -5)]
+        public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
         {
             //Act
-            var result = _sut.Substract(5, 4);
+            var result = _sut.Substract(a, b);
 
             //Assert
-            Assert.Equal(1, result);
+            Assert.Equal(expected, result);
         }
+
+        [Theory]
+        [InlineData(5, 5, 25)]
+        [InlineData(50, 0, 0)]
+        [InlineData(-5, 5, -25)]
+        public void Multiply_ShouldMultiplyTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
+        {
+            //Act
+            var result = _sut.Multiply(a, b);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+
+        [Theory]
+        [InlineData(5, 5, 1)]
+        [InlineData(15, 5, 3)]
+        public void Divide_ShouldDivideTwoNumbers_WhenTwoNumbersAreIntegers(int a, int b, int expected)
+        {
+            //Act
+            var result = _sut.Divide(a, b);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+
+
+
+
 
         [Fact]
         public void Test_Guid_1()
         {
             _outputHelper.WriteLine(_guid.ToString());
         }
-        [Fact]
+        [Fact(Skip = "this is just a test")]
         public void Test_Guid_2()
         {
             _outputHelper.WriteLine(_guid.ToString());
